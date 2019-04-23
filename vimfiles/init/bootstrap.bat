4b70e7b808560f8b2b3006651ab5b8b4f578de1a
mewiteor <mewiteor@hotmail.com>
2019-04-24 02:53:28 +0800

    Update bootstrap.bat

--- vimfiles/init/bootstrap.bat
+++ vimfiles/init/bootstrap.bat
@@ -1,6 +1,6 @@
 @echo off
 git submodule init
 git submodule update
-mkdir ../autoload
-mkdir ../plugged
-copy ../pack/plugins/start/vim-plug/plug.vim ../autoload/plug.vim
+mkdir ..\autoload
+mkdir ..\plugged
+copy ..\pack\plugins\start\vim-plug\plug.vim ..\autoload\plug.vim
